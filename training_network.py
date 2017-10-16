40ebec53d33fb9c5f731cd1a98b094e42ecfab13
ElKebaboKing <ranielmaalouf@gmail.com>
2017-10-16 16:52:34 +0200

    Use dropout at the end of the convolutions.

--- training_network.py
+++ training_network.py
@@ -34,7 +34,7 @@ X_train = np.array(augmented_images)
 y_train = np.array(augmented_measurements)
 
 from keras.models import Sequential, Model
-from keras.layers import Flatten, Dense, Lambda, Cropping2D
+from keras.layers import Flatten, Dense, Lambda, Cropping2D, Dropout, Activation
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 
@@ -48,7 +48,9 @@ model.add(Convolution2D(24,5,5, subsample=(2,2), activation = "relu"))
 model.add(Convolution2D(36,5,5, subsample=(2,2), activation = "relu"))
 model.add(Convolution2D(48,5,5, subsample=(2,2), activation = "relu"))
 model.add(Convolution2D(64,3,3, activation = "relu"))
-model.add(Convolution2D(64,3,3, activation = "relu"))
+model.add(Convolution2D(64,3,3))
+model.add(Dropout(0.5))
+model.add(Activation('relu'))
 
 model.add(Flatten())
 model.add(Dense(100))
