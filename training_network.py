fe1f97f7ba044214f79ae107df885c5b7b0c97a5
ElKebaboKing <ranielmaalouf@gmail.com>
2017-10-16 15:28:38 +0200

    Crop image to only use relevant area in each image, and retrain model.

--- training_network.py
+++ training_network.py
@@ -33,14 +33,14 @@ for image, measurement in zip(images, measurements):
 X_train = np.array(augmented_images)
 y_train = np.array(augmented_measurements)
 
-from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda
+from keras.models import Sequential, Model
+from keras.layers import Flatten, Dense, Lambda, Cropping2D
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 
 model = Sequential()
 model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape = (160,320,3)))
-
+model.add(Cropping2D(cropping=((50,20), (0,0))))
 model.add(Convolution2D(6,5,5,activation="relu"))
 model.add(MaxPooling2D())
 model.add(Convolution2D(6,5,5,activation="relu"))
