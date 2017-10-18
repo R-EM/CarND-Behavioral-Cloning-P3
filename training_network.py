65d6471bf4d242396e4a54d0587df794b8503cba
ElKebaboKing <ranielmaalouf@gmail.com>
2017-10-18 10:18:48 +0200

    Place dropout operation between convolutions and flatten.

--- training_network.py
+++ training_network.py
@@ -77,9 +77,9 @@ model.add(Convolution2D(24,5,5, subsample=(2,2), activation = "relu"))
 model.add(Convolution2D(36,5,5, subsample=(2,2), activation = "relu"))
 model.add(Convolution2D(48,5,5, subsample=(2,2), activation = "relu"))
 model.add(Convolution2D(64,3,3, activation = "relu"))
-model.add(Convolution2D(64,3,3))
+model.add(Convolution2D(64,3,3, activation = "relu"))
+
 model.add(Dropout(0.7))
-model.add(Activation('relu'))
 
 model.add(Flatten())
 model.add(Dense(100))
