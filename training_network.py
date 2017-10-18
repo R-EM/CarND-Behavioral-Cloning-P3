4f2d5ef03e0e18f6693969133464d6d74242e1dd
ElKebaboKing <ranielmaalouf@gmail.com>
2017-10-18 14:15:17 +0200

    Change syntax of image resize.

--- training_network.py
+++ training_network.py
@@ -76,7 +76,7 @@ model = Sequential()
 # Pre-processing of data
 model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape = (160,320,3)))
 model.add(Cropping2D(cropping=((70,25), (0,0))))
-model.add(Lambda(lambda x: resize_img(x)))
+model.add(Lambda(resize_img))
 
 model.add(Convolution2D(24,5,5, subsample=(2,2), activation = "relu"))
 model.add(Dropout(0.5))
