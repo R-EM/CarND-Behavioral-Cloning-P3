597448892771120a172f6b808b4878b8db98efce
ElKebaboKing <ranielmaalouf@gmail.com>
2017-10-20 12:24:15 +0200

    Another test to check whether same network is used between dataset changes.

--- model.py
+++ model.py
@@ -123,7 +123,7 @@ def NVidia_model(model, dropout_rate):
 
 
 dropout_rate = 0.5	
-epochs = 1
+epochs = 2
 
 model = Sequential()
 model = model_preprocessing(model)
@@ -137,6 +137,20 @@ train_generator = generator(train_samples, sample_size, folder_name)
 validation_generator = generator(validation_samples, sample_size, folder_name)
 
 model.fit_generator(train_generator, samples_per_epoch = len(train_samples*6), validation_data = validation_generator, nb_val_samples = len(validation_samples*6), nb_epoch=epochs)
+
+# Load my training samples
+folder_name = './data3/IMG/'
+train_samples, validation_samples = train_test_split(my_samples, test_size=0.2)
+train_generator = generator(train_samples, sample_size, folder_name)
+validation_generator = generator(validation_samples, sample_size, folder_name)
+
+model.fit_generator(train_generator, samples_per_epoch = len(train_samples*6), validation_data = validation_generator, nb_val_samples = len(validation_samples*6), nb_epoch=epochs)
+
+# Load Udacity's training samples
+folder_name = './data_udacity/IMG/'
+train_samples, validation_samples = train_test_split(udacity_samples, test_size=0.2)
+train_generator = generator(train_samples, sample_size, folder_name)
+validation_generator = generator(validation_samples, sample_size, folder_name)
 
 model.fit_generator(train_generator, samples_per_epoch = len(train_samples*6), validation_data = validation_generator, nb_val_samples = len(validation_samples*6), nb_epoch=epochs)
 
