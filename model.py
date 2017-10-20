325e95c242584b2f5cc288609a5c47165881a427
ElKebaboKing <ranielmaalouf@gmail.com>
2017-10-20 14:24:01 +0200

    Use both datasets 3 times.

--- model.py
+++ model.py
@@ -137,21 +137,15 @@ model = model_preprocessing(model)
 model = NVidia_model(model, dropout_rate)
 
 
-# Use Udacity's training samples
-folder_name = './data_udacity/IMG/'
-model = train_model(model, udacity_samples, folder_name, epochs = 1)
+for i in range(3):
+	print("For loop: ", i+1)
+	# Use Udacity's training samples
+	folder_name = './data_udacity/IMG/'
+	model = train_model(model, udacity_samples, folder_name, epochs = 1)
 
-# Use my training samples
-folder_name = './data3/IMG/'
-model = train_model(model, my_samples, folder_name, epochs = 1)
+	# Use my training samples
+	folder_name = './data3/IMG/'
+	model = train_model(model, my_samples, folder_name, epochs = 1)
 
-
-# Use Udacity's training samples
-folder_name = './data_udacity/IMG/'
-model = train_model(model, udacity_samples, folder_name, epochs = 1)
-
-# Use my training samples
-folder_name = './data3/IMG/'
-model = train_model(model, my_samples, folder_name, epochs = 1)
 
 model.save('model.h5')
