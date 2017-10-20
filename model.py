c234e83372027535f7cc014f6e7046d953214240
ElKebaboKing <ranielmaalouf@gmail.com>
2017-10-20 12:17:54 +0200

    Test to see if the same netowrk is being used between datasets.

--- model.py
+++ model.py
@@ -138,6 +138,8 @@ validation_generator = generator(validation_samples, sample_size, folder_name)
 
 model.fit_generator(train_generator, samples_per_epoch = len(train_samples*6), validation_data = validation_generator, nb_val_samples = len(validation_samples*6), nb_epoch=epochs)
 
+model.fit_generator(train_generator, samples_per_epoch = len(train_samples*6), validation_data = validation_generator, nb_val_samples = len(validation_samples*6), nb_epoch=epochs)
+
 # Load my training samples
 folder_name = './data3/IMG/'
 train_samples, validation_samples = train_test_split(my_samples, test_size=0.2)
